0aeef90b8730b951352ac5394658d3495b00ce32
200529-Hugo <78649357+200529-Hugo@users.noreply.github.com>
2021-10-06 09:33:49 +0200

    Update index.php
    Typ fout weg gehaald.

--- DBopdracht1/index.php
+++ DBopdracht1/index.php
@@ -8,12 +8,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>LightSend</title>
+    <title>BIEB</title>
 </head>
 <body>
 <div class="container">
     <header>
-        <h1>Welcome by <b>LightSend</b></h1>
+        <h1>Welcome by <b>BIEB</b></h1>
     </header>
 
 <?php
